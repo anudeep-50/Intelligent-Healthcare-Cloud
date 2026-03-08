409b13149404714c1aa967685545b46b3307df38
anudeep-50 <138959785+anudeep-50@users.noreply.github.com>
2026-03-08 23:28:59 +0530

    Add constants for ECG processing configuration

---
+++ constants.py
@@ -0,0 +1,5 @@
+# constants.py
+WINDOW_SIZE = 100  # Number of ECG points per packet
+SAMPLING_RATE = 360 # MIT-BIH standard Hz
+API_URL = "http://localhost:8000/process"
+ECC_PUBLIC_KEY_PATH = "./keys/public.pem"
